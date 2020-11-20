c6e4ba35bf2fc81c5f00d12f201d41149698dedd
rageroot <rageroot@gmail.com>
2020-11-20 15:38:49 +0300

    Выделение цветом текущей пары rerePush

--- helpers/bells.js
+++ helpers/bells.js
@@ -1,104 +1,6 @@
 'use strict'
 module.exports.info = () => {
-	const dateNow = new Date();
-	const currentHours = dateNow.getHours();
-	let currentState = "";
-
-	let markup = [
-		'<b>1 пара:</b> 08:00 - 09:30\n',
-		'<b>2 пара:</b> 09:40 - 11:10\n',
-		'<b>3 пара:</b> 11:30 - 13:00\n',
-		'<b>4 пара:</b> 13:10 - 14:40\n',
-		'<b>5 пара:</b> 14:50 - 16:20\n',
-		'<b>6 пара:</b> 16:30 - 18:00\n',
-	];
-
-	const markupTime = [		//точность до секунд не важна, используются минуты, прошедшие с начала суток
-		{
-			description: "1 пара",
-			sopMinuts: 570,
-		},
-		{
-			description: "1 перемена",
-			sopMinuts: 580,
-		},
-		{
-			description: "2 пара",
-			sopMinuts: 670,
-		},
-		{
-			description: "2 перемена",
-			sopMinuts: 690,
-		},
-		{
-			description: "3 пара",
-			sopMinuts: 780,
-		},
-		{
-			description: "3 перемена",
-			sopMinuts: 790,
-		},
-		{
-			description: "4 пара",
-			sopMinuts: 880,
-		},
-		{
-			description: "4 перемена",
-			sopMinuts: 890,
-		},
-		{
-			description: "5 пара",
-			sopMinuts: 980,
-		},
-		{
-			description: "5 перемена",
-			sopMinuts: 990,
-		},
-		{
-			description: "6 пара",
-			sopMinuts: 1080,
-		},
-	];
-
-	if((currentHours < 8) || (currentHours > 18)) {
-		if(currentHours < 8)
-			currentState = "Еще слишком рано";
-		else
-			currentState = "Уже слишком поздно";
-	}
-	else{
-		const minutes = currentHours * 60 + dateNow.getMinutes(); //текущее количество минут с начала дня
-		switch (dateNow.getDay()) { //смотрим на расписание, в зависимости от дня недели
-			case 0:
-				currentState = "Сегодня выходной";
-				break;
-			case 6:				//для субботы
-				break;
-			default:			//рабочая неделя
-				for(let i = 0; i < markupTime.length; ++i){			//смотрим какое сейчас событие
-					if(minutes - markupTime[i].sopMinuts < 0){
-						currentState = `Сейчас ${markupTime[i].description}, до конца ${markupTime[i].sopMinuts - minutes} минут`;
-						if(i % 2 == 0){
-							markup[i / 2] = `<span style="color: #ff0000;">${markup[i / 2]}</span>`;
-						}
-						break;
-					}
-				}
-				break;
-		}
-	}
-	markup.push(currentState);
-	//console.log(Array.prototype.join.call(markup,""));
-
-    return new Promise( resolve=>{
-	resolve(Array.prototype.join.call(markup,""));
-    });
-};
-//getDay - номер дня недели
-//getHours()- часы по нашему времени
-
-
-/*const dateNow = new Date();
+const dateNow = new Date();
 const currentHours = dateNow.getHours();
 let currentState = "";
 
@@ -109,7 +11,7 @@ let markup = [
 	'<b>4 пара:</b> 13:10 - 14:40\n',
 	'<b>5 пара:</b> 14:50 - 16:20\n',
 	'<b>6 пара:</b> 16:30 - 18:00\n',
-]/!*.join('\n')*!/;
+];
 
 const markupTime = [		//точность до секунд не важна, используются минуты, прошедшие с начала суток
 	{
@@ -177,7 +79,105 @@ else{
 				if(minutes - markupTime[i].sopMinuts < 0){
 					currentState = `Сейчас ${markupTime[i].description}, до конца ${markupTime[i].sopMinuts - minutes} минут`;
 					if(i % 2 == 0){
-						//markup[i / 2] = `<span style="color: #ff0000;">${markup[i / 2]}</span>`;
+						markup[i / 2] = `<span style="color: #ff0000;">${markup[i / 2]}</span>\n`;
+					}
+					break;
+				}
+			}
+			break;
+	}
+}
+markup.push(currentState);
+console.log(Array.prototype.join.call(markup,""));
+let a = Array.prototype.join.call(markup,"")
+    return new Promise( resolve=>{
+	resolve(a);
+    });
+};
+//getDay - номер дня недели
+//getHours()- часы по нашему времени
+
+
+/*const dateNow = new Date();
+const currentHours = dateNow.getHours();
+let currentState = "";
+
+let markup = [
+	'<b>1 пара:</b> 08:00 - 09:30\n',
+	'<b>2 пара:</b> 09:40 - 11:10\n',
+	'<b>3 пара:</b> 11:30 - 13:00\n',
+	'<b>4 пара:</b> 13:10 - 14:40\n',
+	'<b>5 пара:</b> 14:50 - 16:20\n',
+	'<b>6 пара:</b> 16:30 - 18:00\n',
+];
+
+const markupTime = [		//точность до секунд не важна, используются минуты, прошедшие с начала суток
+	{
+		description: "1 пара",
+		sopMinuts: 570,
+	},
+	{
+		description: "1 перемена",
+		sopMinuts: 580,
+	},
+	{
+		description: "2 пара",
+		sopMinuts: 670,
+	},
+	{
+		description: "2 перемена",
+		sopMinuts: 690,
+	},
+	{
+		description: "3 пара",
+		sopMinuts: 780,
+	},
+	{
+		description: "3 перемена",
+		sopMinuts: 790,
+	},
+	{
+		description: "4 пара",
+		sopMinuts: 880,
+	},
+	{
+		description: "4 перемена",
+		sopMinuts: 890,
+	},
+	{
+		description: "5 пара",
+		sopMinuts: 980,
+	},
+	{
+		description: "5 перемена",
+		sopMinuts: 990,
+	},
+	{
+		description: "6 пара",
+		sopMinuts: 1080,
+	},
+];
+
+if((currentHours < 8) || (currentHours > 18)) {
+	if(currentHours < 8)
+		currentState = "Еще слишком рано";
+	else
+		currentState = "Уже слишком поздно";
+}
+else{
+	const minutes = currentHours * 60 + dateNow.getMinutes(); //текущее количество минут с начала дня
+	switch (dateNow.getDay()) { //смотрим на расписание, в зависимости от дня недели
+		case 0:
+			currentState = "Сегодня выходной";
+			break;
+		case 6:				//для субботы
+			break;
+		default:			//рабочая неделя
+			for(let i = 0; i < markupTime.length; ++i){			//смотрим какое сейчас событие
+				if(minutes - markupTime[i].sopMinuts < 0){
+					currentState = `Сейчас ${markupTime[i].description}, до конца ${markupTime[i].sopMinuts - minutes} минут`;
+					if(i % 2 == 0){
+						markup[i / 2] = `<span style="color: #ff0000;">${markup[i / 2]}</span>\n`;
 					}
 					break;
 				}
