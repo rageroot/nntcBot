f71976426da41649a2ce12a41e26ac91bdddf0aa
rageroot <rageroot@gmail.com>
2020-12-01 15:17:00 +0300

    Добавил декораторы вывода, изменил способ создания нумерации дел для вывода

--- helpers/config.js
+++ helpers/config.js
@@ -1,5 +1,5 @@
-//module.exports.TG_TOKEN = '1041129701:AAGpgjV9wPRvpM3nSaa82i_9aGz1Wci0QUY';//nntc bot
-module.exports.TG_TOKEN = '1421023735:AAE5jGY8v3q6gy1ROsiJqqy0fr5tfHb5rE4'; //rage bot
+module.exports.TG_TOKEN = '1041129701:AAGpgjV9wPRvpM3nSaa82i_9aGz1Wci0QUY';//nntc bot
+//module.exports.TG_TOKEN = '1421023735:AAE5jGY8v3q6gy1ROsiJqqy0fr5tfHb5rE4'; //rage bot
 module.exports.VALID_USERS = [
   '137482679', //svg_52
   '410877972', // avp
