1671d260304c54bc831b9d171b80718c7e3e55b3
rageroot <rageroot@gmail.com>
2020-12-24 11:10:39 +0300

    Заработала генерация odt файлов характеристик

--- helpers/report-generator.js
+++ helpers/report-generator.js
@@ -191,23 +191,68 @@ function cpTemplate(from, to){
 function createCharacteristics(info, paths){
     return new Promise(async (resolve, reject) => {
         let index = 0;
-        for(const student of info.students)
-                try {
-                    await cpTemplate(paths.reportsCharacteristicTemplate, `${paths.tmpFolderPath}/${index}`);
-                    const contentFile = await fsPromises.readFile(`${paths.tmpFolderPath}/${index}/content.xml`);
-                    const totals = await fillingWithRealData(contentFile.toString(), index, info);
-                    await fsPromises.writeFile(`${paths.tmpFolderPath}/${index}/content.xml`, totals)
-                    index++;
-                    resolve();
-                }
-                catch (err) {
-                    reject(new Error("Возникли проблемы с генерацией отчетов"));
-                }
+        for(const student of info.students) {
+            try {
+                await cpTemplate(paths.reportsCharacteristicTemplate, `${paths.tmpFolderPath}/${index}`);
+                const contentFile = await fsPromises.readFile(`${paths.tmpFolderPath}/${index}/content.xml`);
+                const totals = await fillingWithRealData(contentFile.toString(), index, info);
+                await fsPromises.writeFile(`${paths.tmpFolderPath}/${index}/content.xml`, totals)
+                await packer(`${paths.tmpFolderPath}/${index}`, `\'outcome/${info.students[index]}.odt\'`);
+                index++;
+            } catch (err) {
+                reject(new Error("Возникли проблемы с генерацией отчетов"));
+            }
+        }
+        resolve();
     });
 }
 
+/**
+ * Возвращает заполненные реальными данными шаблон
+ * @param template верстка с шаблоном
+ * @param index номер текущей итерации
+ * @param info объект реальных данных
+ * @returns {Promise<unknown>} заполненный шаблон
+ */
 function fillingWithRealData(template, index, info){
    return new Promise(resolve => {
+       //подчеркивание нужной оценки в таблицах
+       const beforeGrade = "<text:span text:style-name=\"T25\">";
+       const afterGrade = "</text:span>";
+       const grades = [0, 1, 2, 3, 4, 5];
+       grades[+info.grades[index]] = beforeGrade + grades[+info.grades[index]] + afterGrade;
+
+       //создание верстки для таблицы с неизвестным заранее количестве ПК
+       const beforePk = "<table:table-row table:style-name=\"Таблица4.2\"><table:table-cell table:style-name=\"Таблица4.A2\" office:value-type=\"string\"><text:p text:style-name=\"P19\">";
+       const afterPk =  "</text:p></table:table-cell><table:table-cell table:style-name=\"Таблица4.A2\" office:value-type=\"string\"><text:p text:style-name=\"P30\">scale</text:p></table:table-cell><table:table-cell table:style-name=\"Таблица4.C2\" office:value-type=\"string\"><text:p text:style-name=\"P27\"/></table:table-cell></table:table-row>";
+       const pk = [];
+       for(const competence of info.pk){
+           pk.push(beforePk + competence + afterPk);
+       }
+
+       //установка маркера в нужной позиции соответсвующих таблиц
+       const gradeMarker = {
+           five: "",
+           four: "",
+           three: "",
+           two: ""
+       };
+
+       switch (info.grades[index]){
+           case '5':
+               gradeMarker.five = "X";
+               break;
+           case '4':
+               gradeMarker.four = "X";
+               break;
+           case '3':
+               gradeMarker.three = "X";
+               break;
+           default:
+               gradeMarker.two = "X";
+               break
+       }
+
        const result = template
            .replace(/\$studentName\$/g, info.students[index])
            .replace(/\$group\$/g, info.group)
@@ -217,7 +262,35 @@ function fillingWithRealData(template, index, info){
            .replace(/\$startDate\$/g, info.begin)
            .replace(/\$endDate\$/g, info.end)
            .replace(/\$leader\$/g, info.leader)
-           .replace(/\$leadPosition\$/g, info.leadPosition);
+           .replace(/\$leadPosition\$/g, info.leadPosition)
+           .replace(/\$f\$/g, gradeMarker.five)
+           .replace(/\$nf\$/g, gradeMarker.four + gradeMarker.three)
+           .replace(/\$no\$/g, gradeMarker.two)
+           .replace(/\$fiv\$/g, gradeMarker.five)
+           .replace(/\$fou\$/g, gradeMarker.four)
+           .replace(/\$thr\$/g, gradeMarker.three)
+           .replace(/\$tw\$/g, gradeMarker.two)
+           .replace(/\$row\$/g, pk.join(""))
+           .replace(/scale/g, grades.join(" "));
        resolve(result);
    });
+}
+
+/**
+ * Запаковывает файли из input в output
+ * @param input
+ * @param output
+ * @returns {Promise<unknown>}
+ */
+function packer(input, output){
+    return new Promise(((resolve, reject) => {
+        const command = `cd ${input}; zip -0 -r ../${output} *`;
+        console.log(command);
+        child_process.exec(command, err => {
+            if(err){
+                reject(new Error("Не могу запаковать в odt"))
+            }
+            resolve();
+        });
+    }));
 }
