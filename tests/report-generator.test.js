8de32d16c1d7a9ff3316b9491ef7a3cb7d3f7767
rageroot <rageroot@gmail.com>
2021-01-13 10:19:16 +0300

    Несколько простых тестов для генератора отчетов

--- tests/report-generator.test.js
+++ tests/report-generator.test.js
@@ -1,5 +1,55 @@
 const reportGenerator = require('../helpers/report-generator');
+const child_process = require('child_process');
+const fsPromises = require('fs').promises;
+const fs = require('fs');
+const axios = require('axios');
 
-describe("Function \"report-generator\"", () => {
+const childProcessExecSpy = jest.spyOn(child_process, 'exec');
 
+userId = 12345;
+
+afterEach(() => {
+    jest.clearAllMocks();
+});
+
+afterAll(() => {
+    jest.restoreAllMocks();
+});
+
+describe("Function \"report-generator\", simple functions", () => {
+    test('get manual', () => {
+        const template = reportGenerator.manual();
+        expect(template).toContain('manual.txt');
+    });
+
+    test('get template', () => {
+        const template = reportGenerator.template();
+        expect(template).toContain('reportTemplate.txt');
+    });
+
+    test('garbageCollector, normal', async () => {
+        childProcessExecSpy.mockImplementationOnce((command, cb) => {
+            cb(null);
+        });
+
+        await reportGenerator.garbageCollector(userId);
+        expect(childProcessExecSpy.mock.calls[0][0]).toBe(`rm -rf tmp/12345_reports`);
+    });
+
+    test('garbageCollector, error', () => {
+        childProcessExecSpy.mockImplementationOnce((command, cb) => {
+            cb(new Error('Jest test error'));
+        });
+
+        reportGenerator.garbageCollector(userId).catch((err) => {
+            expect(err.message).toContain('не могу собрать мусор');
+        });
+    });
+});
+
+
+describe("Function \"report-generator\", generator", () => {
+    test('normal behavior', async () => {
+
+    });
 });
