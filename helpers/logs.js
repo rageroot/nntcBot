0ba07395517434f8554e3d77306f5e8a06ad88b1
rageroot <rageroot@gmail.com>
2021-02-03 11:19:55 +0300

    logs.js- регулярные выражения убирают запятые из заметок о пользователе. Иначе, CSV генерировался не верно

--- helpers/logs.js
+++ helpers/logs.js
@@ -46,7 +46,7 @@ async function generateUsageLog(path){
     for(const log of logs){
         const date = new Date(log.time);
         date.setHours(date.getHours());
-        arrayForCsv.push(`${log.userId},${log.username},${log.realname},${date},${log.messageType},${log.message},${log.note}`);
+        arrayForCsv.push(`${log.userId},${log.username},${log.realname},${date},${log.messageType},${log.message},${log.note.replace(/\$,\$/g,'.')}`);
     }
     await fs.writeFile(path, arrayForCsv.join('\n'));
 }
