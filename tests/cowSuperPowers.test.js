7ca0c004fc2832a7b827bdac22df913701a93e04
rageroot <rageroot@gmail.com>
2021-02-03 16:02:18 +0300

    cowSuperPowers.test.js- первоначальные настройки для тестов cowSuperPower.inputData.js- входные данные

---
+++ tests/cowSuperPowers.test.js
@@ -0,0 +1,43 @@
+const superPower = require('../helpers/cowSuperPowers');
+const users = require('../models/users');
+const mongoose = require('mongoose');
+const inputData = require('../tests/resources/cowSuperPower.inputData');
+
+beforeAll(async () => {
+    await mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true });
+    for(const testUser of inputData.testUsers){
+        await users.newUser(testUser);
+    }
+    /*const tmp = await users.getAllUsers();
+    console.log(tmp);*/
+});
+
+afterEach(() => {
+    jest.clearAllMocks();
+});
+
+afterAll(async () => {
+    jest.restoreAllMocks();
+    await dropAllCollections();
+    await mongoose.connection.close();
+});
+
+describe('module cowSuperPower', () => {
+    describe('function \'getUserInfo\'', () => {
+        test('normal behavior', () => {
+
+        });
+    });
+});
+
+async function dropAllCollections () {
+    const collections = Object.keys(mongoose.connection.collections)
+    for (const collectionName of collections) {
+        const collection = mongoose.connection.collections[collectionName]
+        try {
+            await collection.drop()
+        } catch (err) {
+            console.log(err.message);
+        }
+    }
+}
